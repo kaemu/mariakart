5ccf7f90e0a0999c7b07c396ef9e9c35d0a05c6d
ben <ben@bens.local>
2021-04-16 21:42:31 +0200

    score file

---
+++ src/components/Score.vue
@@ -0,0 +1,16 @@
+<template>
+	<div class="absolute p-8 text-3xl">
+		<p>Maria Kart</p>
+		<p v-for="player, i in players">Player {{ i+1 }}</p>
+	</div>
+</template>
+
+<script setup>
+
+import { computed } from 'vue'
+
+import UseGame from '../uses/useGame.js'
+
+const players = computed(() => UseGame.players.value)
+
+</script>
